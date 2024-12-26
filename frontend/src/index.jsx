10ffc60f2ba3f23b919b1a0de458ed7b3b707ed9
bay <bayeldzhoroev@gmail.com>
2024-12-26 16:52:26 +0300

    Add scss as style source to index

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import init from './init.jsx';
+import './main.scss';
 
 const app = async () => {
     const root = ReactDOM.createRoot(document.querySelector('#chat'));
