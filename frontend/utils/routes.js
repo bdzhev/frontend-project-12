71e415ffa72143e9043c869755716d57bdd01580
bay <bayeldzhoroev@gmail.com>
2024-12-26 16:51:48 +0300

    Add routes for step 2

---
+++ frontend/utils/routes.js
@@ -0,0 +1,11 @@
+const BASE_API_PATH = '/api/v1';
+
+export const apiPaths = {
+  login: () => `${BASE_API_PATH}/login`,
+};
+
+export const linkRoutes = {
+  main: '/',
+  login: '/login',
+  signup: '/signup',
+};
