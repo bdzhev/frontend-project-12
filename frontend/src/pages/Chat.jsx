aaa702d0563ca27734065922ba3027b3ddc37e74
bay <bayeldzhoroev@gmail.com>
2025-01-14 04:45:54 +0300

    Add channel data feature #1

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,13 +1,25 @@
-import { Container, Card } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import channelModals from '../components/modals';
+import { openModal } from '../../store/slices/modalSlice';
+import Channels from '../components/Channels';
 
 const Chat = () => {
+  const type = useSelector((state) => state.modal.type);
+  const dispatch = useDispatch();
+  const handleModal = () => dispatch(openModal({ type: 'add', channel: 'first' }));
   return (
-    <Container>
-      <Card style={{ width: '18rem'}}>
-        <Card.Title>Chat</Card.Title>
-        <Card.Text>Example text</Card.Text>
-      </Card>
-    </Container>
+  <Container fluid className="h-100 my-4">
+  <Row className="flex-md-row h-100 bg-white">
+    <Col className='col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex'>
+      <Channels/>
+    </Col>
+    <Col className='p-0 h-100'>
+      <Button onClick={handleModal}>Open Add Modal</Button>
+    </Col>
+    { !!type ? channelModals[type]() : null }
+  </Row>
+  </Container>
   )
 };
 
--- frontend/store/index.js
+++ frontend/store/index.js
@@ -2,11 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import channelsSliceReducer from "./slices/channelsSlice";
 import messageSliceReducer from "./slices/messageSlice";
 import authSliceReducer from "./slices/authSlice";
+import modalSliceReducer from './slices/modalSlice';
+import { channelsApi } from './services/channelsApi';
 
 export default configureStore({
   reducer: {
     channels: channelsSliceReducer,
     messages: messageSliceReducer,
     auth: authSliceReducer,
+    modal: modalSliceReducer,
+    [channelsApi.reducerPath]: channelsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(channelsApi.middleware),
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
