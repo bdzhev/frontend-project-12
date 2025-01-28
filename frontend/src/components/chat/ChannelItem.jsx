a913625dca37e42ca02c3e769c32fc8e057fd680
bay <bayeldzhoroev@gmail.com>
2025-01-28 23:17:20 +0300

    Fix button role in channels for tests

--- frontend/src/components/chat/ChannelItem.jsx
+++ frontend/src/components/chat/ChannelItem.jsx
@@ -40,6 +40,7 @@ const ChannelItem = ({
               variant={variant}
               onClick={setChannelActive}
               name={name}
+              role="button"
             >
               {`# ${filter.clean(name)}`}
             </Button>
@@ -56,6 +57,7 @@ const ChannelItem = ({
             variant={variant}
             onClick={setChannelActive}
             name={name}
+            role="button"
           >
             {`# ${filter.clean(name)}`}
           </Button>
